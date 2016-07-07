7113d377c839e782e9f05c460d193cb09c198ee6
jtr109 <lyp_login@outlook.com>
2016-07-07 18:59:01 +0800

    add SQL

--- app/main/views.py
+++ app/main/views.py
@@ -2,19 +2,34 @@ from flask import render_template, session, redirect, url_for, flash
 
 from . import main
 from .forms import NameForm
+from ..models import User
+from .. import db
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data)
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('.index'))
+
+        # old_name = session.get('name')
+        # if old_name is not None and old_name != form.name.data:
+        #     flash('Looks like you have changed your name!')
+        # session['name'] = form.name.data
+        # return redirect(url_for('.index'))
+
     return render_template('index.html',
-                           form=form, name=session.get('name'))
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 @main.route('/user/<name>')
--- manage.py
+++ manage.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 
 import os
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Shell
 
-from app import create_app
+from app import create_app, db
+from app.models import Role, User
 
 app = create_app(os.getenv('TODO_CONFIG') or 'default')
 manager = Manager(app)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
     manager.run()
