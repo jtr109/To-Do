cc3bc0ee4897dea50e1fa79bbd20d4240ac92c4f
jtr109 <lyp_login@outlook.com>
2016-07-08 15:35:52 +0800

    add login

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask.ext.login import LoginManager
 
 from config import config
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 mail = Mail()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -18,6 +23,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     mail.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
