4a01dfbc868b9bbbb68579a0b08b6425e00841d3
jtr109 <lyp_login@outlook.com>
2016-07-08 10:56:08 +0800

    add password_hash and blueprint

--- app/main/views.py
+++ app/main/views.py
@@ -24,12 +24,6 @@ def index():
         session['name'] = form.name.data
         return redirect(url_for('.index'))
 
-        # old_name = session.get('name')
-        # if old_name is not None and old_name != form.name.data:
-        #     flash('Looks like you have changed your name!')
-        # session['name'] = form.name.data
-        # return redirect(url_for('.index'))
-
     return render_template('index.html',
                            form=form, name=session.get('name'),
                            known=session.get('known', False))
@@ -38,4 +32,3 @@ def index():
 @main.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
-
