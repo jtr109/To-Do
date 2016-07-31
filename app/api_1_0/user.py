d1304b1d3d16f9116ecbd4485c43d138e4a010c3
jtr109 <lyp_login@outlook.com>
2016-07-31 13:19:05 +0800

    reset user.py

---
+++ app/api_1_0/user.py
@@ -0,0 +1,10 @@
+ flask import jsonify
+
+from ..models import User
+from . import api
+
+
+@api.route('/users/<int:user_id>')
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return jsonify(user.to_json())
