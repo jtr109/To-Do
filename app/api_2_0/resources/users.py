c5fe47e223a0ea46376679068b5c8164c52ff4d4
jtr109 <lyp_login@outlook.com>
2016-08-16 11:46:14 +0800

    add function to show all users

--- app/api_2_0/resources/users.py
+++ app/api_2_0/resources/users.py
@@ -1,3 +1,4 @@
+from flask import url_for
 from flask_restful import Resource
 from app import restful_api
 
@@ -8,11 +9,21 @@ class Test(Resource):
     def get(self, id):
         return {'test': 'success'}
 
-restful_api.add_resource(Test, '/test/<int:id>')
+restful_api.add_resource(Test, '/test/<int:id>', endpoint='Test')
 
-# class UserAPI(Resource):
-#     def get(self, user_id):
-#         return {'user' : 'test'}
-#         # user = User.query.get_or_404(user_id)
-#         # return user.to_json()
 
+class AllUsersAPI(Resource):
+    def get(self):
+        users = User.query.all()
+        users_link = map(lambda user: url_for('api2.UserAPI', user_id=user.id, _external=True), users)
+        return {'users': users_link}
+
+restful_api.add_resource(AllUsersAPI, '/users/', endpoint='AllUsersAPI')
+
+
+class UserAPI(Resource):
+    def get(self, user_id):
+        user = User.query.get_or_404(user_id)
+        return user.to_json()
+
+restful_api.add_resource(UserAPI, '/users/<int:user_id>', endpoint='UserAPI')
